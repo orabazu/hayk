3750ca14ead25392e42265995fa55e4744a70344
zhunor <orhunozor@gmail.com>
2017-02-21 00:44:21 +0300

    Search as i moved

--- client/dist/js/components.js
+++ client/dist/js/components.js
@@ -604,6 +604,32 @@ function FooterController() {
 * @example <div acme-shared-spinner></div>
 */
 angular
+    .module('app.navbar', [])
+    .directive('navbarDirective', navbarDirective);
+   
+function navbarDirective() {
+    var directive = {
+        restrict: 'EA',
+        templateUrl: '../../components/user/navbar/navbar.html',
+        // scope: {
+        //     max: '='
+        // },
+        controller: navbarController,
+        controllerAs: 'vm',
+        bindToController: true
+    };
+
+    return directive;
+}
+
+function navbarController() {
+    var vm = this;
+}
+/**
+* @desc spinner directive that can be used anywhere across apps at a company named Acme
+* @example <div acme-shared-spinner></div>
+*/
+angular
     .module('app.profile', [])
     .directive('profileDirective', profileDirective);
 
@@ -646,32 +672,6 @@ function profileController($rootScope, userService,trackService,markerParser) {
                 });
         });
     }
-}
-/**
-* @desc spinner directive that can be used anywhere across apps at a company named Acme
-* @example <div acme-shared-spinner></div>
-*/
-angular
-    .module('app.navbar', [])
-    .directive('navbarDirective', navbarDirective);
-   
-function navbarDirective() {
-    var directive = {
-        restrict: 'EA',
-        templateUrl: '../../components/user/navbar/navbar.html',
-        // scope: {
-        //     max: '='
-        // },
-        controller: navbarController,
-        controllerAs: 'vm',
-        bindToController: true
-    };
-
-    return directive;
-}
-
-function navbarController() {
-    var vm = this;
 }
 /**
 * @desc spinner directive that can be used anywhere across apps at a company named Acme
